bf387aa827539bf97773a0765d4b36868588a3f9
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-06-30 22:15:21 -0400

    switch to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd stage/_book
+
+git init
+git config user.name "Abigail Cabunoc Mayes"
+git config user.email "abigail@mozillafoundation.org"
+
+git remote add upstream "https://$GH_TOKEN@github.com/mozillascience/leadership-training.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
